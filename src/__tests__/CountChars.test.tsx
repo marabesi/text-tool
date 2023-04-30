91900f9167dfd2035bcf613d90946f3978a04920
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-04-30 11:03:57 +0200

    chore: moving tests around

---
+++ src/__tests__/CountChars.test.tsx
@@ -0,0 +1,29 @@
+import { act, render } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('characters', () => {
+  it('should render text area', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('text-area')).toBeInTheDocument();
+  });
+
+  it('should render zero chars when text area has no text', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Chars: 0')).toBeInTheDocument();
+  });
+
+  it.each([
+    ['mytext', '6'],
+    ['text', '4'],
+  ])('should display the number os characters a word has (%s)', (text: string, chars: string) => {
+    const { getByTestId, getByText } = render(<App />);
+
+    act(() => {
+      userEvent.type(getByTestId('text-area'), text);
+    });
+
+    expect(getByText(`Chars: ${chars}`)).toBeInTheDocument();
+  });
+});
+
---
+++ src/__tests__/CountWords.test.tsx
@@ -0,0 +1,48 @@
+import { act, render } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('words', () => {
+  it('should count word', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('Words: 0')).toBeInTheDocument();
+  });
+
+  it.each([
+    ['my word', '2']
+  ])('should count word %s', (text, count) => {
+    const { getByText, getByTestId } = render(<App />);
+
+    act(() => {
+      userEvent.type(getByTestId('text-area'), text);
+    });
+
+    expect(getByText(`Words: ${count}`)).toBeInTheDocument();
+  });
+
+  it('should reset counters after deleting text', async () => {
+    const { getByTestId, getByText } = render(<App/>);
+
+    act(() => {
+      userEvent.type(getByTestId('text-area'), 'my random text');
+    });
+
+    await userEvent.clear(getByTestId('text-area'));
+
+    expect(getByText('Words: 0')).toBeInTheDocument();
+  });
+
+  it.each([
+    ['my random text ', 3],
+    ['   ', 0],
+  ])('should not count word when next one is space', async (text: string, count: number) => {
+    const { getByTestId, getByText } = render(<App/>);
+
+    act(() => {
+      userEvent.type(getByTestId('text-area'), text);
+    });
+
+    expect(getByText(`Words: ${count}`)).toBeInTheDocument();
+  });
+});
