5917e29f454b97f04b01527435b13c98b790aba9
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-04-30 11:00:34 +0200

    chore: moving tests around

--- src/__tests__/StopWords.test.tsx
+++ src/__tests__/StopWords.test.tsx
@@ -9,39 +9,41 @@ describe('stop words', () => {
     expect(getByPlaceholderText('Stop words: the, an, I')).toBeInTheDocument();
   });
 
-  it.each([
-    // ['my duplicated text my duplicated text', 'my', '1. duplicated: 2'],
-    ['my duplicated text my duplicated text', `my,
+  describe('with stop words enabled',() => {
+    it.each([
+      ['my duplicated text my duplicated text', 'my', '1. duplicated: 2'],
+      ['my duplicated text my duplicated text', `my,
 duplicated`, '1. text: 2'],
-  ])('should ignore stop words', (text, stopWords, expected) => {
-    const { getByTestId, getByText } = render(<App/>);
+    ])('should ignore stop words', (text, stopWords, expected) => {
+      const { getByTestId, getByText } = render(<App/>);
 
-    act(() => {
-      userEvent.type(getByTestId('stop-words-area'), stopWords);
+      act(() => {
+        userEvent.type(getByTestId('stop-words-area'), stopWords);
+      });
+
+      act(() => {
+        userEvent.type(getByTestId('text-area'), text);
+      });
+
+      expect(getByText(expected)).toBeInTheDocument();
     });
 
-    act(() => {
-      userEvent.type(getByTestId('text-area'), text);
+    it('should count words whenever stop words does not exists', () => {
+      const { getByTestId, getByText } = render(<App/>);
+
+      act(() => {
+        userEvent.type(getByTestId('stop-words-area'), 'my');
+      });
+
+      act(() => {
+        userEvent.type(getByTestId('text-area'), 'my duplicated text my duplicated text');
+      });
+
+      act(() => {
+        userEvent.clear(getByTestId('stop-words-area'));
+      });
+
+      expect(getByText('1. my: 2')).toBeInTheDocument();
     });
-
-    expect(getByText(expected)).toBeInTheDocument();
-  });
-
-  it('should count words whenever stop words does not exists', () => {
-    const { getByTestId, getByText } = render(<App/>);
-
-    act(() => {
-      userEvent.type(getByTestId('stop-words-area'), 'my');
-    });
-
-    act(() => {
-      userEvent.type(getByTestId('text-area'), 'my duplicated text my duplicated text');
-    });
-
-    act(() => {
-      userEvent.clear(getByTestId('stop-words-area'));
-    });
-
-    expect(getByText('1. my: 2')).toBeInTheDocument();
   });
 });
