4a080013b55e20dc62934d5e1fefbf8a6ba0dcbd
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-06-11 15:33:15 +0200

    fix: failing tests due stop words

--- src/__tests__/CountWords.test.tsx
+++ src/__tests__/CountWords.test.tsx
@@ -9,16 +9,39 @@ describe('words', () => {
     expect(getByText('Words: 0')).toBeInTheDocument();
   });
 
-  it.each([
-    ['my word', '2']
-  ])('should count word %s', (text, count) => {
-    const { getByText, getByTestId } = render(<App />);
+  describe('with stop words disabled', () => {
+    it.each([
+      ['my word', '2']
+    ])('should count word %s', (text, count) => {
+      const { getByText, getByTestId, getByLabelText } = render(<App />);
 
-    act(() => {
-      userEvent.type(getByTestId('text-area'), text);
+      act(() => {
+        userEvent.click(getByLabelText('Ignore stop words'));
+      });
+
+      act(() => {
+        userEvent.type(getByTestId('text-area'), text);
+      });
+
+      expect(getByText(`Words: ${count}`)).toBeInTheDocument();
     });
 
-    expect(getByText(`Words: ${count}`)).toBeInTheDocument();
+    it.each([
+      ['my random text ', 3],
+      ['   ', 0],
+    ])('should not count word when next one is space', async (text: string, count: number) => {
+      const { getByTestId, getByText, getByLabelText } = render(<App/>);
+
+      act(() => {
+        userEvent.click(getByLabelText('Ignore stop words'));
+      });
+
+      act(() => {
+        userEvent.type(getByTestId('text-area'), text);
+      });
+
+      expect(getByText(`Words: ${count}`)).toBeInTheDocument();
+    });
   });
 
   it('should reset counters after deleting text', async () => {
@@ -31,18 +54,5 @@ describe('words', () => {
     await userEvent.clear(getByTestId('text-area'));
 
     expect(getByText('Words: 0')).toBeInTheDocument();
-  });
-
-  it.each([
-    ['my random text ', 3],
-    ['   ', 0],
-  ])('should not count word when next one is space', async (text: string, count: number) => {
-    const { getByTestId, getByText } = render(<App/>);
-
-    act(() => {
-      userEvent.type(getByTestId('text-area'), text);
-    });
-
-    expect(getByText(`Words: ${count}`)).toBeInTheDocument();
   });
 });
