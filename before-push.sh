fd28302d213c67662ac8c48dfc8d0b81fa39045c
Matheus Marabesi <matheus.marabesi@gmail.com>
2025-02-02 19:26:15 +0100

    chore: updated run script

--- before-push.sh
+++ before-push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker-compose run --rm text-tool rm -rf node_modules/
-docker-compose run --rm text-tool npm i
-docker-compose run --rm text-tool npm run build
-docker-compose run --rm -d text-tool npm run start
+docker compose run --rm text-tool rm -rf node_modules/
+docker compose run --rm text-tool npm i
+docker compose run --rm text-tool npm run build
+docker compose run --rm -d text-tool npm run start
 echo "Sleeping 10 seconds for the application to start"
 sleep 10
 echo "Woke up, after 10 seconds, running tests now..."
-docker-compose run --rm -e CI=true text-tool npm run test
+docker compose run --rm -e CI=true text-tool npm run test
 docker run --rm -v $(pwd):/app -w /app cypress/included:3.2.0 npm run e2e
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
